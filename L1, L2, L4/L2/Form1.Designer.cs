0d4ed34fe547943e684f277380e0725f3e2053a8
Matas Rutkauskas <mrutkauskas@ad.ktug.lt>
2018-05-30 09:54:17 +0300

    update

--- L1, L2, L4/L2/Form1.Designer.cs
+++ L1, L2, L4/L2/Form1.Designer.cs
@@ -37,6 +37,12 @@
             this.baigtiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.skaičiavimaiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pagalbaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.sudarytiRinkiniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.papildytiRinkiniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salintiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -69,24 +75,28 @@
             this.pasirinktiPirmąToolStripMenuItem.Name = "pasirinktiPirmąToolStripMenuItem";
             this.pasirinktiPirmąToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
             this.pasirinktiPirmąToolStripMenuItem.Text = "Pasirinkti pirmą";
+            this.pasirinktiPirmąToolStripMenuItem.Click += new System.EventHandler(this.pasirinktiPirmąToolStripMenuItem_Click);
             // 
             // pasirinktiAntrąToolStripMenuItem
             // 
             this.pasirinktiAntrąToolStripMenuItem.Name = "pasirinktiAntrąToolStripMenuItem";
             this.pasirinktiAntrąToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
             this.pasirinktiAntrąToolStripMenuItem.Text = "Pasirinkti antrą";
+            this.pasirinktiAntrąToolStripMenuItem.Click += new System.EventHandler(this.pasirinktiAntrąToolStripMenuItem_Click);
             // 
             // pasirinktiTrečiąToolStripMenuItem
             // 
             this.pasirinktiTrečiąToolStripMenuItem.Name = "pasirinktiTrečiąToolStripMenuItem";
             this.pasirinktiTrečiąToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
             this.pasirinktiTrečiąToolStripMenuItem.Text = "Pasirinkti trečią";
+            this.pasirinktiTrečiąToolStripMenuItem.Click += new System.EventHandler(this.pasirinktiTrečiąToolStripMenuItem_Click);
             // 
             // spausdintiToolStripMenuItem
             // 
             this.spausdintiToolStripMenuItem.Name = "spausdintiToolStripMenuItem";
             this.spausdintiToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
             this.spausdintiToolStripMenuItem.Text = "Spausdinti";
+            this.spausdintiToolStripMenuItem.Click += new System.EventHandler(this.spausdintiToolStripMenuItem_Click);
             // 
             // baigtiToolStripMenuItem
             // 
@@ -96,6 +106,10 @@
             // 
             // skaičiavimaiToolStripMenuItem
             // 
+            this.skaičiavimaiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sudarytiRinkiniToolStripMenuItem,
+            this.papildytiRinkiniToolStripMenuItem,
+            this.salintiToolStripMenuItem});
             this.skaičiavimaiToolStripMenuItem.Name = "skaičiavimaiToolStripMenuItem";
             this.skaičiavimaiToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
             this.skaičiavimaiToolStripMenuItem.Text = "Skaičiavimai";
@@ -106,11 +120,59 @@
             this.pagalbaToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
             this.pagalbaToolStripMenuItem.Text = "Pagalba";
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(415, 416);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // sudarytiRinkiniToolStripMenuItem
+            // 
+            this.sudarytiRinkiniToolStripMenuItem.Name = "sudarytiRinkiniToolStripMenuItem";
+            this.sudarytiRinkiniToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.sudarytiRinkiniToolStripMenuItem.Text = "Sudaryti rinkini";
+            this.sudarytiRinkiniToolStripMenuItem.Click += new System.EventHandler(this.sudarytiRinkiniToolStripMenuItem_Click);
+            // 
+            // papildytiRinkiniToolStripMenuItem
+            // 
+            this.papildytiRinkiniToolStripMenuItem.Name = "papildytiRinkiniToolStripMenuItem";
+            this.papildytiRinkiniToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.papildytiRinkiniToolStripMenuItem.Text = "Papildyti rinkini";
+            this.papildytiRinkiniToolStripMenuItem.Click += new System.EventHandler(this.papildytiRinkiniToolStripMenuItem_Click);
+            // 
+            // salintiToolStripMenuItem
+            // 
+            this.salintiToolStripMenuItem.Name = "salintiToolStripMenuItem";
+            this.salintiToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.salintiToolStripMenuItem.Text = "Salinti";
+            this.salintiToolStripMenuItem.Click += new System.EventHandler(this.salintiToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(499, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(218, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(496, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Raidė kuria prasidedančias pabaisas pašalinti";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(853, 531);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -133,6 +195,12 @@
         private System.Windows.Forms.ToolStripMenuItem baigtiToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem skaičiavimaiToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem pagalbaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sudarytiRinkiniToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem papildytiRinkiniToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salintiToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- L1, L2, L4/L2/Form1.cs
+++ L1, L2, L4/L2/Form1.cs
@@ -13,14 +13,28 @@ namespace L2
 {
     public partial class Form1 : Form
     {
+        const string fr = "...\\...\\Rezultatai.txt";
+        const string fd1 = "...\\...\\Duom1.txt";
+        const string fd2 = "...\\...\\Duom2.txt";
+        const string fd3 = "...\\...\\Duom3.txt";
+
         List<Pabaisa> Pabaisos1;
         List<Pabaisa> Pabaisos2;
         List<Pabaisa> Pabaisos3;
         List<Pabaisa> Surinktas;
 
+        string miskas1, miskas2, miskas3;
+        string stud1, stud2, stud3;
+
         public Form1()
         {
             InitializeComponent();
+            spausdintiToolStripMenuItem.Enabled = false;
+            pasirinktiAntrąToolStripMenuItem.Enabled = false;
+            pasirinktiTrečiąToolStripMenuItem.Enabled = false;
+            salintiToolStripMenuItem.Enabled = false;
+            papildytiRinkiniToolStripMenuItem.Enabled = false;
+            sudarytiRinkiniToolStripMenuItem.Enabled = false;
         }
 
         static List<Pabaisa> SkaitytiList(string fd, out string studvard, out string mpav)
@@ -31,10 +45,8 @@ namespace L2
             {
                 string eilute;
 
-                eilute = srautas.ReadLine();
-                studvard = eilute;
+                studvard = srautas.ReadLine();
 
-                eilute = srautas.ReadLine();
                 mpav = srautas.ReadLine();
 
                 while ((eilute = srautas.ReadLine()) != null)
@@ -48,6 +60,70 @@ namespace L2
                 }
             }
             return Pabaisos;
+        }
+
+        static void SpausdintiList(string fr, List<Pabaisa> P, string antraste, string pavadinimas)
+        {
+            FileStream fs = new FileStream(fr, FileMode.Append);
+            using (StreamWriter rr = new StreamWriter(fs))
+            {
+                rr.WriteLine(antraste + "\n");
+                rr.WriteLine("|-------------------------|");
+                rr.WriteLine("|{0, -25}|", pavadinimas);
+                rr.WriteLine("|-----------|-------------|---------------|");
+                rr.WriteLine("|Pavadinimas|Ragų skaičius|Uodegų skaičius|");
+                rr.WriteLine("|-----------|-------------|---------------|");
+
+                for (int i = 0; i < P.Count; i++)
+                {
+
+                    rr.WriteLine("|{0,11}|{1,13}|{2,15}|", P[i].pavadinimas, P[i].ragai, P[i].uodegos);
+                    rr.WriteLine("|-----------|-------------|---------------|");
+                }
+                rr.WriteLine();
+            }
+        }
+
+        static void Spausdinti(string fr, List<Pabaisa> P1, List<Pabaisa> P2, string miskas1, string miskas2)
+        {
+            FileStream fs = new FileStream(fr, FileMode.Append);
+            using (StreamWriter rr = new StreamWriter(fs))
+            {
+                int sumragu = 0;
+                int sumuodeg = 0;
+                int indx;
+
+                KiekisMiske(P1, ref sumragu, ref sumuodeg);
+                rr.WriteLine("Miške: {0} yra {1} ragų ir {2} uodegų", miskas1, sumragu, sumuodeg);
+                rr.WriteLine();
+
+                KiekisMiske(P2, ref sumragu, ref sumuodeg);
+                rr.WriteLine("Miške: {0} yra {1} ragų ir {2} uodegų", miskas2, sumragu, sumuodeg);
+                rr.WriteLine();
+
+                indx = MazUodeguota(P1);
+
+                if (indx != -1)
+                {
+                    rr.WriteLine("Mažiausiai uodeguota pabaisa miške {0}\n turi {1} ragų", miskas1, P1[indx].ragai);
+                }
+                else
+                {
+                    rr.WriteLine("Mažiausiai uodeguotos pabaisos miške {0} nėra", miskas1);
+                }
+
+                rr.WriteLine();
+                indx = MazUodeguota(P2);
+
+                if (indx != -1)
+                {
+                    rr.WriteLine("Mažiausiai uodeguota pabaisa miške {0}\n turi {1} ragų", miskas2, P2[indx].ragai);
+                }
+                else
+                {
+                    rr.WriteLine("Mažiausiai uodeguotos pabaisos miške {0} nėra", miskas2);
+                }
+            }
         }
 
         static void KiekisMiske(List<Pabaisa> Pabaisos, ref int sumarag, ref int sumauod)
@@ -95,6 +171,96 @@ namespace L2
             );
         }
 
+        private void spausdintiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            SpausdintiList(fr, Pabaisos1, "Pirmas miskas", miskas1);
+            SpausdintiList(fr, Pabaisos2, "Antras miskas", miskas2);
+            SpausdintiList(fr, Pabaisos3, "Trecias miskas", miskas3);
+            Spausdinti(fr, Pabaisos1, Pabaisos2, miskas1, miskas2);
+            richTextBox1.Text = File.ReadAllText(fr);
+            sudarytiRinkiniToolStripMenuItem.Enabled = true;
+        }
+
+        private void sudarytiRinkiniToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Formuoti(Pabaisos1, Surinktas);
+            Formuoti(Pabaisos2, Surinktas);
+            SpausdintiList(fr, Surinktas, "", "Surinktas rinkinys");
+            Rikiuoti(Surinktas);
+            SpausdintiList(fr, Surinktas, "", "Surikiuotas");
+            richTextBox1.Text = File.ReadAllText(fr);
+            papildytiRinkiniToolStripMenuItem.Enabled = true;
+            salintiToolStripMenuItem.Enabled = true;
+        }
+
+        private void papildytiRinkiniToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Papildyti(Surinktas, Pabaisos3);
+            SpausdintiList(fr, Surinktas, "", "Papildytas surinktas rinkinys");
+            richTextBox1.Text = File.ReadAllText(fr);
+            papildytiRinkiniToolStripMenuItem.Enabled = false;
+        }
+
+        private void salintiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            if (str != "")
+            {
+                Salinti(Surinktas, str[0]);
+                SpausdintiList(fr, Surinktas, "", "Su pasalinta pabaisa");
+                richTextBox1.Text = File.ReadAllText(fr);
+            }
+        }
+
+        private void pasirinktiPirmąToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.Title = "Pasirinkite duomenų failą";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // jeigu pasirinktas failas
+            {
+                string fv = openFileDialog1.FileName;
+                richTextBox1.LoadFile(fv, RichTextBoxStreamType.PlainText);
+                Pabaisos1 = SkaitytiList(fv, out stud1, out miskas1);
+                // Meniu punktų nustatymai
+                pasirinktiAntrąToolStripMenuItem.Enabled = true;
+            }
+        }
+
+        private void pasirinktiAntrąToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.Title = "Pasirinkite duomenų failą";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // jeigu pasirinktas failas
+            {
+                string fv = openFileDialog1.FileName;
+                richTextBox1.LoadFile(fv, RichTextBoxStreamType.PlainText);
+                Pabaisos2 = SkaitytiList(fv, out stud2, out miskas2);
+                // Meniu punktų nustatymai
+                pasirinktiTrečiąToolStripMenuItem.Enabled = true;
+            }
+        }
+
+        private void pasirinktiTrečiąToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.Title = "Pasirinkite duomenų failą";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // jeigu pasirinktas failas
+            {
+                string fv = openFileDialog1.FileName;
+                richTextBox1.LoadFile(fv, RichTextBoxStreamType.PlainText);
+                Pabaisos3 = SkaitytiList(fv, out stud3, out miskas3);
+                // Meniu punktų nustatymai
+                spausdintiToolStripMenuItem.Enabled = true;
+            }
+        }
+
         static void Papildyti(List<Pabaisa> Surinktas, List<Pabaisa> Pabaisos)
         {    
             for (int j = 0; j < Pabaisos.Count; j++)
@@ -108,13 +274,14 @@ namespace L2
 
         static void Prideti(List<Pabaisa> Surinktas, Pabaisa pb)
         {
-            for (int i = 0; i < Surinktas.Count; i++)
+            int k = 0;
+
+            while (k < Surinktas.Count && pb.ragai <= Surinktas[k].ragai)
             {
-                if (pb.ragai > Surinktas[i].ragai)
-                {
-                    Surinktas.Insert(i, pb);
-                }
+                k++;
             }
+
+            Surinktas.Insert(k, pb);
         }
 
         static double UodVidurkis(List<Pabaisa> Pabaisos)
@@ -137,7 +304,7 @@ namespace L2
             {
                 string pav = P[i].pavadinimas;
 
-                if (pav[0] == c)
+                if (char.ToLower(pav[0]) == char.ToLower(c))
                 {
                     P.RemoveAt(i);
                 }
